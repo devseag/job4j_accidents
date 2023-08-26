bf764118cb5a68c3c2208be106de38554cd7a161
devseag <noexample@gmail.com>
2023-08-26 13:22:16 -0300

    add index html load user

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,13 +1,14 @@
 package ru.job4j.accidents.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
-
-    @GetMapping({"/", "/index"})
-    public String getIndex() {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("user", "Bill Johnson");
         return "index";
     }
 }
