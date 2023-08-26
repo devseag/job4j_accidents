156aa781149e836c2a0386e84a061bf406ff53b9
devseag <noexample@gmail.com>
2023-08-26 16:43:48 -0300

    add spring security

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,14 +1,16 @@
 package ru.job4j.accidents.controller;
 
+import org.springframework.security.core.context.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.accidents.repository.*;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
+
+    @GetMapping({"/", "/index"})
     public String index(Model model) {
-        model.addAttribute("user", "Bill Johnson");
         return "index";
     }
 }
--- src/main/java/ru/job4j/accidents/service/MemAccidentService.java
+++ src/main/java/ru/job4j/accidents/service/MemAccidentService.java
@@ -7,52 +7,53 @@ import ru.job4j.accidents.repository.*;
 
 import java.util.*;
 
-@Service
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class MemAccidentService implements AccidentService {
-
-    private AccidentRepository accidentRepository = new MemAccidentRepository();
-
-    @Override
-    public Accident save(Accident accident) {
-        return accidentRepository.save(accident);
-    }
-
-    @Override
-    public void update(Accident accident) {
-        accidentRepository.update(accident);
-    }
-
-    @Override
-    public Collection<Accident> findAll() {
-        return accidentRepository.findAll();
-    }
-
-    @Override
-    public Optional<Accident> findById(int id) {
-        return accidentRepository.findById(id);
-    }
-
-    @Override
-    public Collection<AccidentType> getAccidentTypes() {
-        return accidentRepository.getAccidentTypes();
-    }
-
-    @Override
-    public Collection<Rule> getRules() {
-        return accidentRepository.getRules();
-    }
-
-    @Override
-    public Set<Rule> makeRules(String[] ids) {
-        Set<Rule> rules = new HashSet<>();
-        for (String id : ids) {
-            Rule rule = new Rule();
-            rule.setId(Integer.parseInt(id));
-            rules.add(rule);
-        }
-        return rules;
-    }
+//@Service
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class MemAccidentService {
+//        implements AccidentService {
+//
+//    private AccidentRepository accidentRepository = new MemAccidentRepository();
+//
+//    @Override
+//    public Accident save(Accident accident) {
+//        return accidentRepository.save(accident);
+//    }
+//
+//    @Override
+//    public void update(Accident accident) {
+//        accidentRepository.update(accident);
+//    }
+//
+//    @Override
+//    public Collection<Accident> findAll() {
+//        return accidentRepository.findAll();
+//    }
+//
+//    @Override
+//    public Optional<Accident> findById(int id) {
+//        return accidentRepository.findById(id);
+//    }
+//
+//    @Override
+//    public Collection<AccidentType> getAccidentTypes() {
+//        return accidentRepository.getAccidentTypes();
+//    }
+//
+//    @Override
+//    public Collection<Rule> getRules() {
+//        return accidentRepository.getRules();
+//    }
+//
+//    @Override
+//    public Set<Rule> makeRules(String[] ids) {
+//        Set<Rule> rules = new HashSet<>();
+//        for (String id : ids) {
+//            Rule rule = new Rule();
+//            rule.setId(Integer.parseInt(id));
+//            rules.add(rule);
+//        }
+//        return rules;
+//    }
 }
